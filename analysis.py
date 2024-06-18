c023375b92aea93b81e4ad1a4b694fb57149cf7b
Abhinav271828 <abhinav.m@research.iiit.ac.in>
2024-06-18 11:50:58 +0100

    model loading code'

---
+++ analysis.py
@@ -0,0 +1,14 @@
+import torch
+from model import GPT
+import os
+import pickle as pkl
+
+path = 'results/scratch/m9z0jetl'
+
+state_dict = torch.load(os.path.join(path, 'latest_ckpt.pt'))
+cfg = state_dict['config']
+
+with open(os.path.join(path, 'grammar/PCFG.pkl'), 'rb') as f:
+    pcfg = pkl.load(f)
+model = GPT(cfg.model, pcfg.vocab_size)
+model.load_state_dict(state_dict['net'])
