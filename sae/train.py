652eb8510a9bcaffa9680e8b471b28e530f15ddc
Abhinav271828 <abhinav.m@research.iiit.ac.in>
2024-06-19 10:37:44 +0100

    Add saving code

--- sae/train.py
+++ sae/train.py
@@ -6,26 +6,40 @@ from .data import *
 import argparse
 import wandb
 from tqdm import tqdm
+import json
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--data_path", help="Path to the data directory", type=str)
-parser.add_argument("--model_dir", help="Path to the model directory", type=str, default=None, required=False)
-parser.add_argument("--ckpt", help="Checkpoint file", type=str, default=None, required=False)
-parser.add_argument("--layer_name", help="Layer name", type=str, default=None, required=False)
-parser.add_argument("--num_samples", help="Number of samples", type=int, default=1024, required=False)
-parser.add_argument("--exp_factor", help="Expansion factor", type=int, default=1, required=False)
-parser.add_argument("--alpha", help="L1 regularization coefficient", type=float, default=None, required=False)
-parser.add_argument("--k", help="Sparsity level", type=int, default=-1, required=False)
-parser.add_argument("--lr", help="Learning rate", type=float, default=1e-6, required=False)
-parser.add_argument("--num_epochs", help="Number of epochs", type=int, default=100, required=False)
-parser.add_argument("--patience", help="Patience", type=int, default=3, required=False)
+parser.add_argument("--data_path",   help="Path to the data directory",
+                    type=str)
+parser.add_argument("--model_dir",   help="Path to the model directory",
+                    type=str,        default=None, required=False)
+parser.add_argument("--ckpt",        help="Checkpoint file",
+                    type=str,        default=None, required=False)
+parser.add_argument("--layer_name",  help="Layer name",
+                    type=str,        default=None, required=False)
+parser.add_argument("--num_samples", help="Number of samples",
+                    type=int,        default=1024, required=False)
+parser.add_argument("--batch_size",  help="Batch size",
+                    type=int,        default=32,   required=False)
+parser.add_argument("--exp_factor",  help="Expansion factor",
+                    type=int,        default=1,    required=False)
+parser.add_argument("--alpha",       help="L1 regularization coefficient",
+                    type=float,      default=None, required=False)
+parser.add_argument("--k",           help="Sparsity level",
+                    type=int,        default=-1,   required=False)
+parser.add_argument("--lr",          help="Learning rate",
+                    type=float,      default=1e-6, required=False)
+parser.add_argument("--num_epochs",  help="Number of epochs",
+                    type=int,        default=100,  required=False)
+parser.add_argument("--patience",    help="Patience",
+                    type=int,        default=3,    required=False)
 
 args = parser.parse_args()
 
 train_data = SAEData(args.data_path, args.model_dir, args.ckpt, args.layer_name, args.num_samples)
 dev_data = SAEData(args.data_path, args.model_dir, args.ckpt, args.layer_name, int(0.05 * args.num_samples))
-train_dl = DataLoader(train_data, batch_size=32, shuffle=True)
-val_dl = DataLoader(dev_data, batch_size=32, shuffle=False)
+train_dl = DataLoader(train_data, batch_size=args.batch_size, shuffle=True)
+val_dl = DataLoader(dev_data, batch_size=args.batch_size, shuffle=False)
 
 embedding_size = train_data.activations.size(-1)
 model = SAE(embedding_size, args.exp_factor * embedding_size, k=-1 if args.alpha else args.k)
@@ -72,4 +86,11 @@ for epoch in range(args.num_epochs):
             loss = criterion(recon, activation)
         wandb.log({'val_loss': loss.item()})
 
+i = 0
+while True:
+    dir_name = os.path.join(args.model_dir, f'sae_{i}')
+    if not os.path.exists(dir_name): break
+torch.save(model.state_dict(), os.path.join(dir_name, 'model.pth'))
+with open(os.path.join(dir_name, 'config.json'), 'w') as f:
+    json.dump(vars(args), f, indent=4)
 wandb.finish()
