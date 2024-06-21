d32dbcef0113dc7522f236929a86b607e57fab23
Abhinav271828 <abhinav.m@research.iiit.ac.in>
2024-06-21 18:49:47 +0100

    Fix training loop to work online

--- sae/train.py
+++ sae/train.py
@@ -10,19 +10,17 @@ import json
 def train(args):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    train_data = SAEData(args.data_path, args.model_dir, args.ckpt, args.layer_name, args.num_samples, is_val=False, device=device)
-    val_data = SAEData(args.data_path, args.model_dir, args.ckpt, args.layer_name, int(0.05 * args.num_samples), is_val=True, device=device)
-    train_dl = DataLoader(train_data, batch_size=args.batch_size, shuffle=True)
-    val_dl = DataLoader(val_data, batch_size=args.batch_size, shuffle=False)
-    print("Data loaded.")
+    train_data = SAEData(args.model_dir, args.ckpt, args.layer_name, device=device)
+    val_data = SAEData(args.model_dir, args.ckpt, args.layer_name, device=device)
+    train_dl = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, collate_fn=train_data.collate_fn)
+    val_dl = DataLoader(val_data, batch_size=args.batch_size, shuffle=False, collate_fn=val_data.collate_fn)
 
-    embedding_size = train_data.activations.size(-1)
+    embedding_size = train_data[0][0].size(-1)
     model = SAE(embedding_size, args.exp_factor * embedding_size, k=-1 if args.alpha else args.k).to(device)
     model.train()
 
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
     criterion = nn.MSELoss()
-
 
     # Train the SAE
     wandb.init(project="sae")
@@ -30,12 +28,16 @@ def train(args):
     wandb.run.save()
     wandb.config.update(vars(args))
 
-    loss_increasing = 0
-    prev_loss = float('inf')
-    for epoch in range(args.num_epochs):
-        model.train()
+    for epoch in range(1):
+        loss_increasing = 0
+
         train_loss = 0
-        for seq_id, activation in tqdm(train_dl, desc="Training"):
+        prev_loss = float('inf')
+
+        train_it = 0
+        for activation, seq in tqdm(train_dl, desc="Training", total=args.train_iters):
+            if train_it > args.train_iters: break
+
             activation = activation.to(device)
             optimizer.zero_grad()
             latent, recon = model(activation)
@@ -43,30 +45,36 @@ def train(args):
             loss += args.alpha * torch.norm(latent, p=1) if args.alpha else 0
             loss.backward()
             optimizer.step()
-            train_loss += loss.item()
+            train_loss = loss.item()
 
+            if loss > prev_loss: loss_increasing += 1
+            else: loss_increasing = 0
 
-        if loss > prev_loss: loss_increasing += 1
-        else: loss_increasing = 0
+            if loss_increasing == args.patience: break
+            prev_loss = loss
 
-        if loss_increasing == args.patience: break
-        prev_loss = loss
-
-        model.eval()
-        val_loss = 0
-        with torch.no_grad():
-            for seq_id, activation in tqdm(val_dl, desc="Validating"):
-                activation = activation.to(device)
-                latent, recon = model(activation)
-                loss = criterion(recon, activation)
-                val_loss += loss.item()
-
-        wandb.log({'train_loss': train_loss / len(train_dl),
-                   'val_loss'  : val_loss   / len(val_dl)  })
+            if train_it % args.val_interval == 0:
+                model.eval()
+                val_loss = 0
+                val_it = 0
+                with torch.no_grad():
+                    for activation, seq in val_dl:
+                        if val_it > args.val_iters: break
+                        activation = activation.to(device)
+                        latent, recon = model(activation)
+                        loss = criterion(recon, activation)
+                        val_loss += loss.item()
+                        val_it += 1
+                model.train()
+                wandb.log({'train_loss': train_loss,
+                           'val_loss'  : val_loss   / args.val_iters})
+            else:
+                wandb.log({'train_loss': train_loss})
+            train_it += 1
 
     i = 0
     while True:
-        dir_name = os.path.join(args.model_dir if args.model_dir else args.data_path, f'sae_{i}')
+        dir_name = os.path.join(args.model_dir, f'sae_{i}')
         if not os.path.exists(dir_name): break
         i += 1
     os.mkdir(dir_name)
