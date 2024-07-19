b6bac89dfa4bc573c4e917455947252199e38ee1
Abhinav271828 <abhinav.m@research.iiit.ac.in>
2024-07-19 15:37:24 +0100

    Fix model arch

--- sae/model.py
+++ sae/model.py
@@ -7,15 +7,17 @@ class SAE(nn.Module):
         super(SAE, self).__init__()
         self.k = k                 # Optional[int]
 
-        self.encoder = nn.Sequential(nn.Linear(input_dim, hidden_dim, bias=False), nn.ReLU()) if not sparsemax else \
+        self.bias = nn.Parameter(torch.zeros(input_dim))
+        self.encoder = nn.Sequential(nn.Linear(input_dim, hidden_dim, bias=True), nn.ReLU()) if not sparsemax else \
                        SimpleLatentProto(input_dim, hidden_dim)
-        self.decoder = nn.Linear(hidden_dim, input_dim, bias=False)
+        self.decoder = nn.Parameter(torch.randn(hidden_dim, input_dim)) #nn.Linear(hidden_dim, input_dim, bias=False)
     
     def forward(self, x):
-        latent = self.encoder(x)
+        latent = self.encoder(x - self.bias)
         if self.k:
             values, indices = torch.topk(latent, self.k)
             latent = torch.zeros_like(latent)
             latent.scatter_(-1, indices, values)
-        recon = self.decoder(latent)
+        norm = torch.norm(self.decoder, p=2, dim=1)
+        recon = torch.matmul(latent, self.decoder / norm.unsqueeze(1)) + self.bias
         return latent, recon
